9764e3024b361626eda233f40077fcb1d45dd040
Alibek <79817284+abdr4@users.noreply.github.com>
2022-07-15 03:19:55 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './main.css';
+
+function App() {
+  return (
+    <div className="w-72 h-96 bg-gray-800 flex justify-center">
+      <h1 className="text-3xl text-white pt-6">Get started!</h1>
+    </div>
+  );
+}
+
+export default App;
